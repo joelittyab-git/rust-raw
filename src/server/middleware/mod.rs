a0698895e4ab5a410b514f85d44d05770c0b8abc
joelittyab-git <joeittab@gmail.com>
2024-08-03 23:46:44 +0530

    cfg [dev-feature]

--- src/server/middleware/mod.rs
+++ src/server/middleware/mod.rs
@@ -1,7 +1,7 @@
-use std::net::TcpStream;
-
+use std::{fmt::Debug, net::TcpStream};
 ///Trait to implement middlewares in the call stack
 #[cfg(feature="developement")]
-pub trait Middleware{
+pub trait Middleware:Debug{
      fn intercept(incoming:TcpStream)->TcpStream;
 }
+
