6a2fe03fd60bda316fdfc3dc984a5aeaab88c970
joelittyab-git <joeittab@gmail.com>
2024-08-07 22:44:09 +0530

    tests[chore]

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,6 +3,24 @@ use std::{io::{Read, Write}, net::TcpStream, thread, time::Duration};
 pub fn def_client(){
      let mut  c = TcpStream::connect("localhost:5000").expect("Something went wrong while client tried to connect to server");
      c.write("SEND;scale".as_bytes()).expect("sOMETHING WENT WRONG");
-
+     let str_buf = format!("scale-rand\n\rSome data is here");
+     let buf = str_buf.as_bytes();
      thread::sleep(Duration::from_secs(6));
+     c.write(buf).expect("Something went wrong while sending data");
+     thread::sleep(Duration::from_secs(10));
+     let mut  read_buf = [0;1028];
+     c.read(&mut read_buf).expect("Something went wrong while reading from server...");
+     println!("{}", String::from_utf8_lossy(&read_buf));
+     c.shutdown(std::net::Shutdown::Both).expect("Something went wrong while trying to shutdown stream");
+}
+
+pub fn create_client(username:String){
+     let mut  c = TcpStream::connect("localhost:5000").expect("Something went wrong while client tried to connect to server");
+     let x = format!("RECEIVE;{username}");
+     c.write(x.as_bytes()).expect("sOMETHING WENT WRONG");
+     thread::sleep(Duration::from_secs(10));
+     let mut  read_buf = [0;1028];
+     c.read(&mut read_buf).expect("Something went wrong while reading from server...");
+     println!("{}", String::from_utf8_lossy(&read_buf));
+     c.shutdown(std::net::Shutdown::Both).expect("Something went wrong while trying to shutdown stream");
 }
--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,15 @@ fn main(){
      if(inp.contains('a')){
           let mut server = Server::new("localhost".to_string(), 5000);
           server.serve().expect("seving went wrong");
-     }else{
+     }else if inp.trim().replace("\n", "")=="s"{
           client::def_client();
+     }else if inp.trim().replace("\n", "")=="r"{
+          println!("Enter username: ");
+          let mut buf = String::new();
+          stdin().read_line(&mut buf).expect("Something went wrong while reading from stdin");
+          buf = buf.trim().to_string();
+          buf = buf.replace("\n", "");
+          client::create_client(buf);
      }
 
 
