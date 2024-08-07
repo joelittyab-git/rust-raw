71084e996f69aed3e9562ec22586754afd57ede1
joelittyab-git <joeittab@gmail.com>
2024-08-07 18:46:11 +0530

    test-client thread::sleep() [chore]

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,6 +1,8 @@
-use std::{io::Write, net::TcpStream};
+use std::{io::{Read, Write}, net::TcpStream, thread, time::Duration};
 
 pub fn def_client(){
      let mut  c = TcpStream::connect("localhost:5000").expect("Something went wrong while client tried to connect to server");
      c.write("SEND;scale".as_bytes()).expect("sOMETHING WENT WRONG");
+
+     thread::sleep(Duration::from_secs(6));
 }
